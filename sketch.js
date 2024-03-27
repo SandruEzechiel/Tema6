cc29a1b0823516debb09d3a8166542eea9273ffb
SandruEzechiel <146414741+SandruEzechiel@users.noreply.github.com>
2024-03-27 16:33:40 +0200

    Final

--- sketch.js
+++ sketch.js
@@ -3,16 +3,16 @@ function setup() {
 }
 
 function draw() {
-  background(135, 206, 235); // Cer albastru
+  background(135, 206, 235); 
 
 
 
-  // Soare
-  fill(255, 255, 0); // Soare galben
+  
+  fill(255, 255, 0); 
   ellipse(200, 200, 80, 80); 
   
-  // Raze de soare
-  fill(255, 255, 0); // Raze galbene
+
+  fill(255, 255, 0); 
   for (let i = 0; i < 12; i++) {
     let angle = TWO_PI / 12 * i;
     let rayX = 200 + cos(angle) * 40;
@@ -20,26 +20,26 @@ function draw() {
     triangle(200, 200, rayX, rayY, 200 + cos(angle + PI/8) * 60, 200 + sin(angle + PI/8) * 60);
   }
   
-  // Dimensiuni casa
+ 
   let casaW = 200;
   let casaH = 150;
   
-  // Poziționare casa în centrul ecranului
+ 
   let casaX = width / 2 - casaW / 2;
   let casaY = height / 2 - casaH / 2;
   
-  // Casa
-  fill(255, 0, 0); // Casa rosie
-  rect(casaX, casaY, casaW, casaH); // Corpul casei
+
+  fill(255, 0, 0); 
+  rect(casaX, casaY, casaW, casaH); 
   
-  fill(0, 255, 0); // Acoperis verde
-  triangle(casaX, casaY, casaX + casaW, casaY, casaX + casaW / 2, casaY - casaW / 2); // Acoperis
+  fill(0, 255, 0); 
+  triangle(casaX, casaY, casaX + casaW, casaY, casaX + casaW / 2, casaY - casaW / 2); 
   
-  fill(255); // Fereastra alba
-  rect(casaX + 50, casaY + 50, 50, 50); // Fereastra
+  fill(255); 
+  rect(casaX + 50, casaY + 50, 50, 50); 
   
-  fill(139, 69, 19); // Ușa maro
-  rect(casaX + 120, casaY + 75, 50, 75); // Ușa
+  fill(139, 69, 19); 
+  rect(casaX + 120, casaY + 75, 50, 75); 
 }
 
 
